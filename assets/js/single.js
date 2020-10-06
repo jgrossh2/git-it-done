7fb6e0a39304c4d20746f0299c0906878ec7e57e
Mona Lisa <joannagrosshans2@gmail.com>
2020-10-06 09:21:49 -0500

    redirect to homepage

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,11 +1,12 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-
-    fetch(apiUrl).then(function(response) {
-        //request was successful
-        if (response.ok) {
+    //make a get request to url
+        fetch(apiUrl).then(function(response) {
+            //request was successful
+            if (response.ok) {
             response.json().then(function(data) {
                 // pass response data to dom function
                 displayIssues(data);
@@ -16,9 +17,26 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            //if not successful redirect to homepage
+            document.location.replace("./index.html");
         }
     });
+};
+
+var getRepoName = function() {
+    //grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    console.log(repoName);
+
+    if (repoName) { 
+        // display repo name on page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html")
+    }
 };
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -66,4 +84,4 @@ var displayIssues = function(issues) {
     };
 
 
-getRepoIssues("facebook/react");
+getRepoName();
